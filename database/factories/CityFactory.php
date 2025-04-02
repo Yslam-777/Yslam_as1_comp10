fd07e7346fa5ad62a4d6f8bbe8bc2dc6e1449c88
Yslam-777 <islamyalkab@gmail.com>
2025-04-02 17:26:54 +0500

    1.2.1

--- database/factories/CityFactory.php
+++ database/factories/CityFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\City;
-
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\City>
