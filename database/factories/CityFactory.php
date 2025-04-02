5def4e45c058b41705958270aebbf590a2467b6a
Yslam-777 <islamyalkab@gmail.com>
2025-04-02 17:27:48 +0500

    1.3.0

--- database/factories/CityFactory.php
+++ database/factories/CityFactory.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class CityFactory extends Factory
 {
     protected $model = City::class;
-    public function definition()
+
     {
         return [
             "name" => $this->faker->city,
